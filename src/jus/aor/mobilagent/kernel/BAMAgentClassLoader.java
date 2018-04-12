b371c196e6220f7eae50f09ed7541dcce8f59f96
Sekina <Sekina@PC-Sekina>
2018-04-12 22:37:59 +0200

    BAMAgentClassLoader done !

--- src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
+++ src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
@@ -4,35 +4,76 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 
 public class BAMAgentClassLoader extends ClassLoader {
+
+	/**
+	 * Fichier Jar de l'agent.
+	 */
+	Jar jar;
+
+	/**
+	 * Création du Loader pour l'agent. L'ensemble des classes est contenu dans le jar
+	 * 
+	 * @param jarName, le nom du fichier jar
+	 */
 	BAMAgentClassLoader(String jarName, ClassLoader parent) {
+		super(parent);
 		try {
-			Jar jar = new Jar(jarName);
-			Iterator<Entry<String, byte[]>> it = jar.classIterator().iterator();
-			while (it.hasNext()) {
-				Entry<String, byte[]> next = it.next();
-				super.defineClass(next.getKey(), next.getValue(), 0, next.getValue().length);
-			}
+			integrateCode(new Jar(jarName));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Création du Loader pour l'agent sans fichier jar.
+	 */
+	public BAMAgentClassLoader(ClassLoader parent) {
+		super(parent);
+	}
+
+	/** 
+	 * Définition des classes présentes dans le jar.
+	 * @param jar
+	 */
 	void integrateCode(Jar jar) {
-		// TODO
+		this.jar = jar;
+		Iterator<Entry<String, byte[]>> it = jar.classIterator().iterator();
+		while (it.hasNext()) {
+			Entry<String, byte[]> next = it.next();
+			super.defineClass(className(next.getKey()), next.getValue(), 0, next.getValue().length);
+		}
 	}
 
+
+	/**
+	 * renvois le jar.
+	 */
 	Jar extractCode() {
-		// TODO
-		return null;
+		return jar;
 	}
-
+	
+	/**
+	 * renvois le nom du jar et les noms des classes qu'il contient.
+	 */
 	public String toString() {
-		// TODO
-		return null;
+		String string;
+		string = jar.toString() + "contains : \n";
+		Iterator<Entry<String, byte[]>> it = jar.classIterator().iterator();
+		while (it.hasNext()) {
+			Entry<String, byte[]> next = it.next();
+			string = string + className(next.getKey()) + "\n";
+		}
+		return string + "END \n";
 	}
 
-	private String className(String arg1) {
-		// TODO
-		return null;
+	/**
+	 * remplace le séparateur de niveau par le séparateur de package.
+	 * 
+	 * @param name le nom physique de la classe
+	 * @return le nom logique associé au nom physique de la classe.
+	 */
+	private String className(String name) {
+		// On affiche le nom sans '.class' et en remplassant '/' par '.'
+		return name.substring(0, name.length()-6).replace('/', '.');
 	}
 }
